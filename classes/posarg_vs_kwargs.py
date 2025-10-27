0a307a90f43d1f06491bcb48e1045b7d07c8ac2d
riprayt <riprayt@gmail.com>
2025-10-27 17:06:07 +0300

    add f2 function to demonstrate handling of positional and keyword arguments

--- classes/posarg_vs_kwargs.py
+++ classes/posarg_vs_kwargs.py
@@ -1,9 +1,23 @@
 def somefunc(x: int, *, y: int) -> None:
     print(f"x: {x}, y: {y}")
 
+def f2(*args, **kwargs) -> None:
+
+    print("args:", args, type(args))
+    print("kwargs:", kwargs, type(kwargs))
+
+    for i in args:
+        print("arg:", i)
+    for k, v in kwargs.items():
+        print(f"kwarg: {k} = {v}")
+
+
+
 if __name__ == "__main__":
     # correct way to call it
     somefunc(1, y=2)
 
     # incorrect way to call it
     # somefunc(1, 2)  # this will raise a TypeError
+
+    f2(1, 2, a=3, b=4)
